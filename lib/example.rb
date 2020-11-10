82c933ddf36a0e33c225a643aca40cfa0f96d683
Aiden <aiden.marques94@gmail.com>
2020-11-09 20:04:01 -0500

    commit tree 71680bcc0960601c1e4c0075667252ce04063dc1

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,15 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_10_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
